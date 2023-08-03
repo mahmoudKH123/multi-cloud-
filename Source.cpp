ee7cce3cdd2d9c7b90ec579702c6d238aa361fc2
mahmoudKH123 <50171298+mahmoudKH123@users.noreply.github.com>
2023-08-03 03:46:23 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+const float pi = 3.142;
+float computearea(float radius);
+
+float computearea(float radius) {
+	float result;
+	result = radius * radius*pi;
+	return result;
+}
+int main() {
+	float r, a;
+	cout << "User, please enter the value of r" << endl;
+	cin >> r;
+	a = computearea(r);
+	cout << "the area is:" << a<<endl;
+	system("pause");
+	return 0;
+}

a939d66f10c2c448f13bed914b4b34d556f15d83
bassem ehab <bassemelgen500@gmail.com>
2023-08-09 12:53:30 +0300

    network

--- AWS/storage.tf
+++ AWS/storage.tf
@@ -23,3 +23,32 @@ resource "aws_volume_attachment" "OrangeAttachment" {
   volume_id = aws_ebs_volume.OrangeVolume.id
   instance_id = aws_instance.OrangeVM.id
 }
+
+resource "aws_vpc" "myvpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "My terraform vpc "
+    }
+}
+resource "aws_subnet" "public subnet" {
+    vpc_id = aws_vpc.myvpc.vpc_id
+    cidr_block = "10.0.1.0/24"
+}
+resource "aws_subnet" "private subnet" {
+    vpc_id = aws_vpc.myvpc.vpc_id
+    cidr_block = "10.0.2.0/24"
+}
+resource "aws_internet_gateway" "igw"{
+    vpc_id = "aws_vpc.myvpc.id"
+}
+resource "aws_route_table" "public RT"{
+    vpc_id = "aws_vpc.myvpc.id"
+    route{
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+}
+resource "aws_route_table_association" "publicRT association"{
+subnet_id = aws_subnet.publicsubnet.id
+route_table_id = aws_route_table.publicRT.id 
+}
